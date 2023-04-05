3d363d6600b7ed57c1787304c2605449cfe01f25
Allysson Ribeiro <allyssonribeiro1@gmail.com>
2023-04-05 17:17:05 -0300

    add global style and eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./styles/themes/default";
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './global'
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <h1>hello word</h1>
+      <GlobalStyle />
     </ThemeProvider>
-  );
+  )
 }
