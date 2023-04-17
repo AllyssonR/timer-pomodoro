e0f402e8d3071d2b0b7e9a54a55122969aa74cae
Allysson Ribeiro <allyssonribeiro1@gmail.com>
2023-04-17 17:35:03 -0300

    add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './global'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>hello word</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
