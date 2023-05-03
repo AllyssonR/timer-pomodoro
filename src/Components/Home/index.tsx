4b17ce9fdd7026ab719c86131b536eeb89a202de
Allysson Ribeiro <allyssonribeiro1@gmail.com>
2023-05-02 23:40:59 -0300

    add react-hook-form and zod
    Validate form made with react-hook-form  and validate it with zod

--- src/Components/Home/index.tsx
+++ src/Components/Home/index.tsx
@@ -8,11 +8,28 @@ import {
   TaskInput,
   MinutesAmountInput,
 } from './styles'
-
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O Ciclo precisa ser no minimo 5 minutos')
+    .max(60, 'O intervalo precisa ser no máximo 60 minutos'),
+})
 export function Home() {
+  const { register, handleSubmit, watch } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+  })
+  function handleCreateNewCycle(data: any) {
+    console.log(data)
+  }
+  const task = watch('task')
+  const isSubmitDisabled = !task
   return (
     <HomeContainer>
-      <form action="">
+      <form action="" onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
@@ -20,6 +37,7 @@ export function Home() {
             type="text"
             placeholder="Dê um nome para seu projeto"
             list="task-suggestions"
+            {...register('task')}
           />
           <datalist id="task-suggestions">
             <option value="Projeto 1" />
@@ -36,6 +54,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
           <span>minutos.</span>
         </FormContainer>
@@ -47,7 +66,7 @@ export function Home() {
           <span>0</span>
           <span>0</span>
         </CountDownContainer>
-        <StartCountDownButton>
+        <StartCountDownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           <span>Começar</span>
         </StartCountDownButton>
